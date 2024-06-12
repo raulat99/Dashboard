0a0110977f54e19de41da61cec27c1dd725f1fd4
RAÚL ARROYO TORRIJOS <raulat99@gmail.com>
2024-06-12 19:27:31 +0200

    Added editable markers

--- src/components/ControlsVideo.tsx
+++ src/components/ControlsVideo.tsx
@@ -25,10 +25,11 @@ import { ValidationResult } from 'joi';
 import { markersValidationSchema } from '../models/import-markers-validation';
 import { VideoContext } from '../providers/VideoProvider';
 import { useSession } from 'next-auth/react';
+import CrudModal from './AddMarkerModal';
 
 export default function ControlsVideo(props: any) {
   const { percentageX, dataX } = useContext(DashboardGraphsContext);
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalAddMarkerVisible, setIsModalAddMarkerVisible] = useState(false);
 
   const {
     videoSync,
@@ -117,7 +118,7 @@ export default function ControlsVideo(props: any) {
   };
 
   const toggleModal = () => {
-    setIsModalVisible(!isModalVisible);
+    setIsModalAddMarkerVisible(!isModalAddMarkerVisible);
   };
 
   // const durationTimeCode = getTimeCode(Math.ceil(duration));
@@ -498,115 +499,15 @@ export default function ControlsVideo(props: any) {
               <IoMdAdd size={24} />
             </button>
 
-            <div
-              id='crud-modal'
-              aria-hidden='true'
-              className={`absolute bottom-full right-0 z-50 mb-2 flex items-center justify-center ${isModalVisible ? '' : 'hidden'}`}
-            >
-              <div className='relative max-h-full w-max	 p-4'>
-                <div className='relative rounded-lg bg-white shadow dark:bg-gray-700'>
-                  <div className='flex items-center justify-between rounded-t border-b p-4 md:p-5 dark:border-gray-600'>
-                    <h3 className='text-lg font-semibold text-gray-900 dark:text-white'>
-                      Create New Marker
-                    </h3>
-                    <button
-                      type='button'
-                      onClick={toggleModal}
-                      className='ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white'
-                    >
-                      <svg
-                        className='h-3 w-3'
-                        aria-hidden='true'
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 14 14'
-                      >
-                        <path
-                          stroke='currentColor'
-                          strokeLinecap='round'
-                          strokeLinejoin='round'
-                          strokeWidth='2'
-                          d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
-                        />
-                      </svg>
-                      <span className='sr-only'>Close modal</span>
-                    </button>
-                  </div>
-                  <div className='p-4 md:p-5'>
-                    <div className='mb-4 grid grid-cols-2 gap-4 '>
-                      <div className='col-span-2'>
-                        <label
-                          htmlFor='name'
-                          className='mb-2 block text-sm font-medium text-gray-900 dark:text-white'
-                        >
-                          Name
-                        </label>
-                        <input
-                          type='text'
-                          name='name'
-                          id='name'
-                          ref={inputTitleMarker}
-                          className='focus:ring-primary-600 focus:border-primary-600 dark:focus:ring-primary-500 dark:focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400'
-                          placeholder='Type marker name'
-                          required
-                        />
-                      </div>
-                      <div className='col-span-2'>
-                        <label
-                          htmlFor='description'
-                          className='mb-2 block text-sm font-medium text-gray-900 dark:text-white'
-                        >
-                          Marker Description
-                        </label>
-                        <textarea
-                          id='description'
-                          rows={4}
-                          ref={inputDescriptionMarker}
-                          className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500'
-                          placeholder='Write marker description here'
-                        ></textarea>
-                      </div>
-                    </div>
-                    <button
-                      type='submit'
-                      className='inline-flex items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-                      onClick={onAddMarkerClick}
-                    >
-                      <svg
-                        className='-ms-1 me-1 h-5 w-5'
-                        fill='currentColor'
-                        viewBox='0 0 20 20'
-                        xmlns='http://www.w3.org/2000/svg'
-                      >
-                        <path
-                          fillRule='evenodd'
-                          d='M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z'
-                          clipRule='evenodd'
-                        ></path>
-                      </svg>
-                      Add new marker
-                    </button>
-                </div>
-                </div>
-              </div>
-            </div>
+            <CrudModal
+        isVisible={isModalAddMarkerVisible}
+        toggleModal={toggleModal}
+        onAddMarkerClick={onAddMarkerClick}
+        inputTitleMarker={inputTitleMarker}
+        inputDescriptionMarker={inputDescriptionMarker}
+      />
           </div>
-          {/* <div>
-            <input
-              type='text'
-              ref={inputTitleMarker}
-              id='first_name'
-              className='-mt-1 block h-8 w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-white dark:focus:border-blue-500 dark:focus:ring-blue-500'
-              placeholder='Marker title'
-              required
-            />
-          </div> */}
-
-          {/* <button className='add-marker' onClick={onAddMarkerClick}>
-            <div className='h-8 w-10'>
-              <IoMdAdd size={24} />
-            </div>
-          </button> */}
+         
           <button className='delete-marker' onClick={onDeleteMarkerClick}>
             <div className='relative h-8 w-10'>
               <TiDeleteOutline size={24} />
@@ -650,7 +551,12 @@ export default function ControlsVideo(props: any) {
         </div>
 
         <div className="display flex mr-auto">
-        <NavbarMarkers markers={markers} handleMarkerClick={navbarMarkerClick} currentTime={currentTimeProgressBar}  />
+        <NavbarMarkers 
+          markers={markers} 
+          handleMarkerClick={navbarMarkerClick} 
+          currentTime={currentTimeProgressBar}
+          updateMarkersDatabase={updateMarkersDatabase}
+          updateMarkers={updateMarkers}  />
         </div>
 
         
--- src/components/NavbarMarkers.tsx
+++ src/components/NavbarMarkers.tsx
@@ -1,29 +1,76 @@
 'use client'
 import { Marker } from "@/models/Marker";
-import { useState } from "react";
+import { MouseEventHandler, useRef, useState } from "react";
+import EditMarkerModal from "./EditMarkerModal";
 
 
 interface NavbarMarkersProps {
 
     markers: Marker[];
     handleMarkerClick: (marker: Marker) => void;
+    updateMarkersDatabase: (markers: Marker[]) => void;
+    updateMarkers : (markers: Marker[]) => void;
     currentTime: number;
 
 }
 
 export default function NavbarMarkers(props: NavbarMarkersProps) {
 
-    const { markers, handleMarkerClick, currentTime } = props;
-    const [isModalVisible, setIsModalVisible] = useState(false);
+    const { markers, handleMarkerClick, currentTime, updateMarkers, updateMarkersDatabase } = props;
+    const [isNavbarModalVisible, setIsNavbarModalVisible] = useState(false);
+    const [isModalEditMarkerVisible, setIsModalEditMarkerVisible] = useState(false);
+    const [markerIdToEdit, setMarkerIdToEdit] = useState<number>("");
 
-    const toggleModal = () => {
-        setIsModalVisible(!isModalVisible);
+    const inputTitleMarker = useRef<HTMLInputElement>(null);
+    const inputDescriptionMarker = useRef<HTMLTextAreaElement>(null);
+
+    const switchNavbarModalVisibility = () => {
+        setIsNavbarModalVisible(!isNavbarModalVisible);
+        if(isNavbarModalVisible) setIsModalEditMarkerVisible(!isNavbarModalVisible);
       };
     
+      const switchEditMarkerModalVisibility = () => {
+        setIsModalEditMarkerVisible(!isModalEditMarkerVisible);
+      };
+
     const sortedMarkers = markers.sort((a, b) => a.time - b.time);
 
     console.log(sortedMarkers)
     console.log(currentTime)
+
+
+    const handleEditMarker = (e: React.MouseEvent<HTMLButtonElement>,  markerTitle?: string, markerDescription?:string, markerId?: number )=>{
+      e.stopPropagation(); 
+      console.log("hola")
+
+      console.log(markerTitle, markerDescription, markerId)
+
+      inputTitleMarker.current!.value = markerTitle!;
+      inputDescriptionMarker.current!.value = markerDescription!;
+      setMarkerIdToEdit(markerId!);
+      setIsModalEditMarkerVisible(true)
+    }
+
+    const editButtonClick = () => {
+
+        const newMarkers = markers.map((marker) => {
+            if(marker.id === markerIdToEdit){
+                return {
+                    ...marker,
+                    title: inputTitleMarker.current!.value,
+                    description: inputDescriptionMarker.current!.value
+                }
+            }
+            return marker;
+        });
+
+        console.log(newMarkers)
+
+        updateMarkersDatabase(newMarkers);
+        updateMarkers(newMarkers);
+        setIsModalEditMarkerVisible(false);
+
+    }
 
     return (
     <div>
@@ -31,15 +78,15 @@ export default function NavbarMarkers(props: NavbarMarkersProps) {
               <button
                 className='rounded-lg bg-blue-700 px-5 py-1.5  text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
                 type='button'
-                onClick={toggleModal}
+                onClick={switchNavbarModalVisibility}
               >
-                { !isModalVisible ? "Show Markers" : "Hide Markers"}
+                { !isNavbarModalVisible ? "Show Markers" : "Hide Markers"}
               </button>
             </div>
 
-        <div id="drawer-navigation"  className={` ${isModalVisible ? '' : 'hidden'} fixed top-16 left-64 z-50 w-64 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white dark:bg-gray-800`} tabIndex={-1} aria-labelledby="drawer-navigation-label">
+        <div id="drawer-navigation"  className={` ${isNavbarModalVisible ? '' : 'hidden'} fixed top-16 left-64 z-50 w-64 h-screen p-4 overflow-y-auto  overflow-x-hidden	  transition-transform -translate-x-full bg-white dark:bg-gray-800`} tabIndex={-1} aria-labelledby="drawer-navigation-label">
           <h5 id="drawer-navigation-label" className="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Makers</h5>
-          <button type="button" onClick={toggleModal} data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+          <button type="button" onClick={switchNavbarModalVisibility} data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
             <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             <span className="sr-only">Close menu</span>
           </button>
@@ -57,19 +104,43 @@ export default function NavbarMarkers(props: NavbarMarkersProps) {
 
             {sortedMarkers && sortedMarkers.map((marker: Marker, i: number) => {
                 return (
-                    <li className={`border rounded-md ${currentTime - 0.1 < marker.time && marker.time < currentTime + 0.1 ? 'border-green-500' : 'border-white-500' } `}  key={i}>
-                        <button onClick={() => handleMarkerClick(marker)} className="flex flex-col flex-start p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group w-full">
-                            <p className="text-white text-xl"> {marker.title} </p>
+                    <li className={`border	 rounded-md ${currentTime - 0.1 < marker.time && marker.time < currentTime + 0.1 ? 'border-green-500' : 'border-white-500' } `}  key={i}>
+                        <button onClick={() => handleMarkerClick(marker)} className="flex flex-col flex-start p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group w-full z-20">
+                            <div className="display flex flex-row space-x-1 place-content-between w-full">
+                            <div>
+                            <p className="  text-white text-xl text-ellipsis overflow-hidden ..."> {marker.title} </p>
+                              </div> 
+                            <button className=" z-50 hover:blue-800 active:bg-blue-700 focus:ring focus:ring-blue-300" onClick={(e)=>{handleEditMarker(e, marker.title, marker.description, marker.id)}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                                  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                              </svg>
+                            </button>
+
+                            </div>
+                            
                             <p className="text-white text-sm"> at {marker.time.toFixed(2)}</p>
-                            <p className="text-white text-md text-start"> {marker.description}</p>
+                            <div className="w-48 ">
+                            <p className="text-white text-md text-start break-words"> {marker.description}</p>
+                            </div>
+                            
                         </button>
                     </li>
                 );
             })}
              
             </ul>
+
+            
           </div>
         </div>
+
+        <EditMarkerModal
+        isVisible={isModalEditMarkerVisible}
+        toggleModal={switchEditMarkerModalVisibility}
+        editButtonClick={editButtonClick}
+        inputTitleMarker={inputTitleMarker}
+        inputDescriptionMarker={inputDescriptionMarker}
+      />
         </div>
     )
 
